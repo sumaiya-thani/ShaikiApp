a4988e6650e1c70a33c088fd025a462f5d2c1ba7
sumaiya-thani <sumaiyathani@gmail.com>
2023-11-30 06:01:43 +0300

    Initial Commit

---
+++ Shaiki/ShaikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaikiApp.swift
+//  Shaiki
+//
+//  Created by sumaiya on 30/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ShaikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
